20ef3ff2b2d23041a5b79ff0e6c20d6253542989
Praveensskp <30658951+Praveensskp@users.noreply.github.com>
2017-08-02 10:00:36 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("http://logos.textgiraffe.com/logos/logo-name/Hai-designstyle-friday-m.png");  
+  res.send("Article one requested and will be served here");  
 });
 app.get('/article-two', function (req, res) {
   res.send("Article two requested and will be served here");  
