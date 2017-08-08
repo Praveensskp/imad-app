f35f29be3163fc2b0644b4341b9c4b117902e164
Praveensskp <30658951+Praveensskp@users.noreply.github.com>
2017-08-08 06:02:24 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter=0;
-app.get('counter', function(req,res){
+var counter = 0;
+app.get('counter', function (req,res){
   counter=counter + 1;
-  res.send(counter.toSrting());
+  res.send(counter.toString());
 });
+
 app.get('/article-one', function (req, res) {
   res.send("Article one requested and will be served here");  
 });
