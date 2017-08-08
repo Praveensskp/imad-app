92666877517120c83bfe5470eb844502005b26ac
Praveensskp <30658951+Praveensskp@users.noreply.github.com>
2017-08-08 05:59:28 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('counter', function(req,res){
+  counter=counter + 1;
+  res.send(counter.toSrting());
+});
 app.get('/article-one', function (req, res) {
   res.send("Article one requested and will be served here");  
 });
